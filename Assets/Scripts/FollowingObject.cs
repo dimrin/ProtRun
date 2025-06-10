56925756dcdbfcbbe76672efd2c5e6dab5f1ec4e
dimrin <zuka537122@gmail.com>
2025-06-10 15:04:31 +0300

    General Improvements

--- Assets/Scripts/FollowingObject.cs
+++ Assets/Scripts/FollowingObject.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowingObject : MonoBehaviour
-{
+public class FollowingObject : MonoBehaviour, IFollowable {
     // Start is called before the first frame update
     [SerializeField] private Transform target;
     private Vector3 offset;
@@ -14,6 +13,11 @@ public class FollowingObject : MonoBehaviour
 
     // Update is called once per frame
     void LateUpdate()
+    {
+        Follow();
+    }
+
+    public void Follow()
     {
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
         transform.position = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
