d472b404a1badb6efee6e302fc130ad4d14ab77f
dimrin <zuka537122@gmail.com>
2025-06-10 16:05:36 +0300

    pick object

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour, IPickable {
+    public void Picked()
+    {
+        Debug.Log("Pick");
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/Player/PlayerItemPicker.cs
+++ Assets/Scripts/Player/PlayerItemPicker.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerItemPicker : MonoBehaviour
 {
-    
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.TryGetComponent<IPickable>(out IPickable pickable))
+        {
+            pickable.Picked();
+        }
+        
+    }
 }
