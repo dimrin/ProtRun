e417fdab1d953ca12dd77280fdb4aa11bb037aef
dimrin <zuka537122@gmail.com>
2025-06-12 17:38:54 +0300

    PlayerHealth Test

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private bool isHittable = true;
+
+
+    public void GetHit(GameObject gameObject, Action OnGetHit)
+    {
+        if (gameObject.TryGetComponent(out IHittable hittable))
+        {
+            hittable.Hit();
+            if(isHittable)
+            {
+                OnGetHit?.Invoke();
+            }
+        }
+    }
+
+
+    public void MakeUnhittable(int timerTime)
+    {
+        StartCoroutine(ChangeHittableState(timerTime));
+    }
+
+    private IEnumerator ChangeHittableState(int timerTime)
+    {
+        isHittable = false;
+        yield return new WaitForSeconds(timerTime);
+        isHittable = true;
+    }
+}
